4758fef03326465d1dc970f493acad58f5b9cfcd
reshals <36654357+reshals@users.noreply.github.com>
2024-01-17 12:48:36 -0300

    Update belladonna.py
    Added retrieve.object() function

--- v0.1/belladonna.py
+++ v0.1/belladonna.py
@@ -122,6 +122,14 @@ class belladonna:
 				objects = os.listdir(column_dir)
 			return objects
 
+		def object(self, object_name, column_name, table_name, database_name, outer_class):
+			object_file = f"{outer_class.db_folder}/{database_name}/{table_name}/{column_name}/{object_name}"
+			if not os.path.exists(object_file):
+				print("Object does not exist!")
+				return 1
+			else:
+				with open(object_file, 'r') as object_file:
+					return object_file.read()
 #example usage 
 if __name__ == "__main__":
 	db = belladonna("~/belladonna/dbs/test")
